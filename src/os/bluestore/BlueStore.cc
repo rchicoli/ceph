64e6aec95fa08212c30d2c7c261a1e539709a655
xie xingguo <xie.xingguo@zte.com.cn>
2016-01-06 16:40:19 +0800

    BlueStore: fix shared_ptr leak during fsck
    Signed-off-by: xie xingguo <xie.xingguo@zte.com.cn>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -1903,8 +1903,10 @@ int BlueStore::fsck()
   if (bluefs) {
     used_blocks.insert(bluefs_extents);
     r = bluefs->fsck();
-    if (r < 0)
+    if (r < 0) {
+      coll_map.clear();
       goto out_alloc;
+    }
     if (r > 0)
       errors += r;
   }
